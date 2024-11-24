92f28496b35731f626ed8613b195cf3a01d760b1
Shivam Mahajan <shivammahajan1908@gmail.com>
2024-11-24 11:19:47 +0530

    invoice producer

---
+++ producer/invoice_producer.py
@@ -0,0 +1,31 @@
+from gc import callbacks
+
+from confluent_kafka import Producer
+import json
+import time
+
+
+class InvoiceProducer:
+    def __init__(self):
+        self.topic = "invoices"
+        self.conf = {'bootstrap.servers': '',
+                     'security.protocol': 'SASL_SSL',
+                     'sasl.mechanism': 'PLAIN',
+                     'sasl.username': '',
+                     'sasl.password': '',
+                     'client.id': ""}
+
+    def delivery_callback(self, err, msg):
+        if err:
+            print('ERROR: Message  failed delivery: {}'.format(err))
+        else:
+            key = msg.key().decode('utf-8')
+            invoice_id = json.loads(msg.value().decode('utf-8'))["InvoiceNumber"]
+            print(f"Produced event to : key = {key} value = {invoice_id}")
+
+    def produce_invoices(self, producer):
+        with open("data/invoices.json") as lines:
+            for line in lines:
+                invoice = json.loads(line)
+                store_id = invoice["StoreID"]
+                producer.produce(self, key=store_id, value=line, callbacks=self.delivery_callback())
