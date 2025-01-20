224c2668b4ccec6dfae3ca3e14bdd7f75d5ce717
Shivam Mahajan <shivammahajan1908@gmail.com>
2025-01-20 23:27:49 +0530

    Increase producer throughput

--- kafka_with_java/kafka_wikimedia_stream_producer/src/main/java/dataensols/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka_with_java/kafka_wikimedia_stream_producer/src/main/java/dataensols/kafka/wikimedia/WikimediaChangesProducer.java
@@ -24,6 +24,15 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        //for kakfa < = 2.8.0
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG,Integer.toString(Integer.MAX_VALUE));
+
+        //For increased throughput
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG,"20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
 
         //Creating the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
